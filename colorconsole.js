5d08bd7e9d20871883471f84e42f8cf3c9325617
kenblikylee <kenblikylee@126.com>
2019-10-08 09:52:45 +0800

    add colorconsole package.

---
+++ colorconsole.js
@@ -0,0 +1,100 @@
+/**
+  * MIT License
+  * Copyright (c) 2019 kenblikylee
+  * https://github.com/kenblikylee/colorconsole
+*/
+
+var colorconsole = (function (logfuc) {
+    var fgcolors = {
+        black: 30,
+        red: 31,
+        green: 32,
+        yellow: 33,
+        blue: 34,
+        magenta: 35,
+        cyan: 36,
+        white: 37
+    }
+    var bgcolors = {
+        black: 40,
+        red: 41,
+        green: 42,
+        yellow: 43,
+        blue: 44,
+        magenta: 45,
+        cyan: 46,
+        white: 47
+    }
+    var effects = {
+        bold: 1,
+        underline: 4,
+        blink: 5,
+        reverse: 7,
+        hide: 8
+    }
+    function is_colornum (color) {
+        if (color = parseInt(color), !isNaN(color)) {
+            if (color >= 0 && (color <= 255)) {
+                return true
+            }
+        }
+        return false
+    }
+    function convert_to_colornum (color) {
+        if (color = parseInt(color), !isNaN(color)) {
+            if (color >= 0 && (color <= 255)) {
+                return color
+            } else {
+                return Math.abs(color) % 256
+            }
+        }
+        return false
+    }
+    function colortext (text, color, bgcolor, effect) {
+        if (!text) return text
+
+        var vals = []
+        if (color) {
+            if (color in fgcolors) {
+                vals.push(fgcolors[color])
+            } else {
+                color = convert_to_colornum(color)
+                if (color !== false) {
+                    vals.push("38;5;" + color)
+                }
+            }
+        }
+        if (bgcolor) {
+            if (bgcolor in bgcolors) {
+                vals.push(bgcolors[bgcolor])
+            } else {
+                bgcolor = convert_to_colornum(bgcolor)
+                if (color !== false) {
+                    vals.push("48;5;" + bgcolor)
+                }
+            }
+        }
+        if (effect && (effect in effects)) {
+            vals.push(effects[effect])
+        }
+        if (vals.length) {
+            var prefix = "\u001b["
+            var reset = prefix + "0m"
+            text = prefix + vals.join(';') + 'm' + text + reset
+        }
+        return text
+    }
+    function colorlog (text, color, bgcolor, effect) {
+        var text = colortext(text, color, bgcolor, effect)
+        var args = [].slice.call(arguments, 4)
+        // args.unshift(text)
+        args.push(text)
+        return logfuc.apply(null, args)
+    }
+    return {
+        text: colortext,
+        log: colorlog
+    }
+})(console.log)
+
+module.exports = colorconsole
---
+++ test.js
@@ -0,0 +1,29 @@
+var colorconsole = require('./colorconsole')
+
+colorconsole.log('hello world!', 'red', null, 'bold')
+
+colorconsole.log('hello world!', 'green', null, 'underline')
+
+colorconsole.log('hello world!', 'blue', null, 'blink')
+
+colorconsole.log('hello world!', 'magenta', null, 'hide')
+
+colorconsole.log('hello world!', 'yellow', null, 'reverse')
+
+colorconsole.log('hello world!', 'magenta', 'cyan')
+
+colorconsole.log('hello world!', 228, null, 'blink')
+
+colorconsole.log('hello world!', 9, 228, 'blink')
+
+colorconsole.log('hello world!', null, null, 'reverse')
+
+colorconsole.log('hello world!', 300, 600)
+
+colorconsole.log('hello world!', 100)
+
+colorconsole.log('hello world!', null, 100)
+
+colorconsole.log('hello world!', 200, 300, 'blink', 'I\'m %s!', 'ken')
+
+console.log(colorconsole.text('hello world!', 100, null, 'bold'), 'I\'m ken!')
